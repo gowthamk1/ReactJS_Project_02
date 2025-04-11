de5671e50d298534a91c2e3f21bd28a925155cdb
gowthamkpvt@gmail.com <gowthamkpvt@gmail.com>
2025-04-11 11:40:14 +0530

    Re-added BrandPage as a regular folder after removing .git

---
+++ BrandPage/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navigation from "./Componants/Navigation";
+import Center from "./Componants/Center";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Navigation />
+        <Center />
+      </div>
+    </div>
+  );
+};
+
+export default App;
